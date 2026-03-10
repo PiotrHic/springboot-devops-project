c6945f2879e1690f9996aa5e6de5cb67c1ba2d86
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-03-10 09:22:29 +0100

    Reast Controller created

---
+++ src/main/java/org/example/springbootdevopsproject/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.springbootdevopsproject;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String, String> hello() {
+        return Map.of("message", "Hello DevOps!");
+    }
+}
