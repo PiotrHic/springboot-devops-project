58b5f96e8a61cbd2ba78fb97e2734e6a550d8e63
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-03-10 09:20:00 +0100

    Initial Commit

---
+++ src/test/java/org/example/springbootdevopsproject/SpringbootDevopsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootdevopsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDevopsProjectApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
